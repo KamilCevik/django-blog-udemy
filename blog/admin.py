2ff15ba59303ed6a2c0677e8d0ee4e3070931058
Kamil Ahmet Çevik <kamilahmetcevik@gmail.com>
2022-02-12 06:26:38 +0300

    yorum modeli ve admin paneli düzenlenmesi tamamlandı.

--- blog/admin.py
+++ blog/admin.py
@@ -1,15 +1,21 @@
 from re import search
 from django.contrib import admin
-from blog.models import KategoriModel, YazilarModel
+from blog.models import KategoriModel, YazilarModel, YorumModel
 
 admin.site.register(KategoriModel)
 
 
 class YazilarAdmin(admin.ModelAdmin):
-    search_fields=('baslik','icerik')
-    list_display = (
-        'baslik', 'olusturulma_tarihi', 'duzenleme_tarihi'
-        )
+    search_fields = ('baslik', 'icerik')
+    list_display = ('baslik', 'olusturulma_tarihi', 'duzenleme_tarihi')
 
 
 admin.site.register(YazilarModel, YazilarAdmin)
+
+
+class YorumAdmin(admin.ModelAdmin):
+    list_display = ("yazan", "olusturulma_tarihi", "guncellenme_tarihi")
+    search_fields = ("yazan__username", )
+
+
+admin.site.register(YorumModel, YorumAdmin)
